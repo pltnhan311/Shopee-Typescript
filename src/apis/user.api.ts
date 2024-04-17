235ed0ae98ba3ac0b2ac86664dec81e9679e3c3b
pltnhan311 <pltnhan311@gmail.com>
2024-04-17 17:21:38 +0700

    declare related Api for User Profile

---
+++ src/apis/user.api.ts
@@ -0,0 +1,24 @@
+import { Omit } from 'lodash'
+import { User } from '~/types/user.type'
+import { SuccessResponse } from '~/types/utils.type'
+import Http from '~/utils/http'
+
+interface BodyUpdateProfile extends Omit<User, '_id' | 'roles' | 'email' | 'createdAt' | 'updatedAt'> {
+  password?: string
+  newPassword?: string
+}
+
+export const getProfile = async () => {
+  return await Http.get<SuccessResponse<User>>('me')
+}
+
+export const updateProfile = async (body: BodyUpdateProfile) => {
+  return await Http.put<SuccessResponse<User>>('user', body)
+}
+
+export const uploadAvatar = async (body: FormData) => {
+  return await Http.post<SuccessResponse<string>>('user/upload-avatar', body, {
+    // khi dung form data phai truyen cai nay len
+    headers: { 'Content-Type': 'multipart/form-data' }
+  })
+}
